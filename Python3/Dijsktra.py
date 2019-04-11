0e3f227ddc766bf7b961bc5916b5bb21ea4ceb47
CucchiettiNicola <46776635+CucchiettiNicola@users.noreply.github.com>
2019-04-11 09:54:48 +0200

    Add files via upload

--- Python3/Dijsktra.py
+++ Python3/Dijsktra.py
@@ -1,32 +1,32 @@
 import math
 
 grafo = [[0, 1, 4, 0, 0, 0, 0, 0],
-         [1, 0, 0, 0, 0, 2, 0, 0],
+         [1, 0, 0, 0, 0, 2, 0, 1],
          [4, 0, 0, 5, 0, 0, 0, 0],
          [0, 0, 5, 0, 1, 3, 0, 0],
          [0, 0, 0, 1, 0, 0, 2, 3],
          [0, 2, 0, 3, 0, 0, 0, 0],
          [0, 0, 0, 0, 2, 0, 0, 4],
-         [0, 0, 0, 0, 3, 0, 4, 0]]
+         [0, 1, 0, 0, 3, 0, 4, 0]]
 
 labelList = [math.inf] * len(grafo)  # inizializzazione
 labelList[0] = 0  # distanza da nodo sorgente uguale a 0
-contatore = 0
 
 nodiDaEsplorare = [0, 1, 2, 3, 4, 5, 6, 7]
 while len(nodiDaEsplorare) > 0:
+    # stampo la labelList (lista con i pesi del percorso per raggiungere quel nodo)
     print(labelList)
-    # Scelgo il nodo che ha la label più piccola (k).
+    # trovo il nodo con la label più piccola in labelList e rimuovo il nodo da nodiDaEsplorare.
     nodoMinore = math.inf
-    for k in labelList:
-        if labelList.index(k) in nodiDaEsplorare:
-            if nodoMinore > k:
-                lblMinore = k
-                nodoMinore = labelList.index(k)
+    for indice,contenuto in enumerate(labelList):
+        if indice in nodiDaEsplorare:
+            if nodoMinore > contenuto:
+                lblMinore = contenuto
+                nodoMinore = indice
     nodiDaEsplorare.remove(nodoMinore)
-    # Ricerco i nodi collegati al nodo k -> ciclo for sulla riga k di grafo
-    for k in grafo[nodoMinore]:
-        if k > 0:
-            if grafo[nodoMinore].index(k) in nodiDaEsplorare:
-                if labelList[grafo[nodoMinore].index(k)] > k + lblMinore:
-                    labelList[grafo[nodoMinore].index(k)] = k + lblMinore
+    # inserisco nella labelList i nodi collegati al nodo estratto
+    for indice,contenuto in enumerate(grafo[nodoMinore]):
+        if contenuto > 0:
+            if indice in nodiDaEsplorare:
+                if labelList[indice] > contenuto + lblMinore:
+                    labelList[indice] = contenuto + lblMinore
---
+++ Python3/Enumerate.py
@@ -0,0 +1,5 @@
+lista = ["ciao", "mi", "chiamo", "lollo", "barcollo", "ma", "non", "mollo", "io", "lo", "dico", "cosi"]
+
+for k,item in enumerate(lista):
+    print(k)
+    print(item)
