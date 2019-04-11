523652b4c2bfb52c7883364c76b36819f9a12da5
CucchiettiNicola <46776635+CucchiettiNicola@users.noreply.github.com>
2019-04-11 10:07:20 +0200

    Add files via upload

--- Python3/Dijsktra.py
+++ Python3/Dijsktra.py
@@ -1,32 +1,37 @@
 import math
 
 grafo = [[0, 1, 4, 0, 0, 0, 0, 0],
-         [1, 0, 0, 0, 0, 2, 0, 1],
+         [1, 0, 0, 0, 0, 2, 0, 0],
          [4, 0, 0, 5, 0, 0, 0, 0],
          [0, 0, 5, 0, 1, 3, 0, 0],
          [0, 0, 0, 1, 0, 0, 2, 3],
          [0, 2, 0, 3, 0, 0, 0, 0],
          [0, 0, 0, 0, 2, 0, 0, 4],
-         [0, 1, 0, 0, 3, 0, 4, 0]]
+         [0, 0, 0, 0, 3, 0, 4, 0]]
 
 labelList = [math.inf] * len(grafo)  # inizializzazione
 labelList[0] = 0  # distanza da nodo sorgente uguale a 0
 
 nodiDaEsplorare = [0, 1, 2, 3, 4, 5, 6, 7]
+cntPasso = 0
 while len(nodiDaEsplorare) > 0:
-    # stampo la labelList (lista con i pesi del percorso per raggiungere quel nodo)
-    print(labelList)
+    # stampo il numero del passo e la labelList (lista con i pesi del percorso per raggiungere quel nodo)
+    print("Passo numero: %d", cntPasso)
+    print("Nodiinesplorati: ", nodiDaEsplorare)
+    print("Lista delle label: ", labelList)
+    print("-----------------------------------------------------------------------")
     # trovo il nodo con la label più piccola in labelList e rimuovo il nodo da nodiDaEsplorare.
     nodoMinore = math.inf
-    for indice,contenuto in enumerate(labelList):
+    for indice, contenuto in enumerate(labelList):
         if indice in nodiDaEsplorare:
             if nodoMinore > contenuto:
                 lblMinore = contenuto
                 nodoMinore = indice
     nodiDaEsplorare.remove(nodoMinore)
     # inserisco nella labelList i nodi collegati al nodo estratto
-    for indice,contenuto in enumerate(grafo[nodoMinore]):
+    for indice, contenuto in enumerate(grafo[nodoMinore]):
         if contenuto > 0:
             if indice in nodiDaEsplorare:
                 if labelList[indice] > contenuto + lblMinore:
                     labelList[indice] = contenuto + lblMinore
+    cntPasso = cntPasso + 1
